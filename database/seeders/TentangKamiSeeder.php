8b2d57c53e6ab6ed139fa5d5c4206ff6d8d4a06a
yellowkunn <ahsanlubis308@gmail.com>
2025-10-25 18:27:57 +0700

    update

--- database/seeders/TentangKamiSeeder.php
+++ database/seeders/TentangKamiSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Aboutus;
+use App\Models\AboutUs;
 
 class TentangKamiSeeder extends Seeder
 {
